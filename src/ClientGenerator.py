6f5bb17494964814cf7b9028ff5a206b72d9e387
Nicolas <nicolas.went@epita.fr>
2023-05-12 18:16:15 +0200

    [Archi] Verified files

--- src/ClientGenerator.py
+++ src/ClientGenerator.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from argparse import Namespace
 
 
@@ -15,16 +18,28 @@ class ClientGenerator:
         self._dest_folder = arguments.dest
 
     def _verify_args(self, arguments: Namespace):
-        """Verify if the arguments are correct. Throws an Exception if they are
-        not.
+        """Verify if the arguments are correct. It prints an error if the arguments
+        are wrong and exit the program with error code.
+        
+        Here are the error code used:
+        https://stackoverflow.com/questions/1101957/are-there-any-standard-exit-status-codes-in-linux
         
         Correct arguments currently means:
         - arguments.file exist
         - arguments.dest does not exist, or if it exists, it must be an empty folder
 
+        exit the program with 66 (cannot open) if arguments.file does not exist
+        exit the program with 73 (can't create user output file) if arguments.dest is a non-empty folder or a file
+
         :param arguments: The program arguments that we have to verify
         :type arguments: Namespace
         """
+        if not os.path.isfile(arguments.file):
+            print(f"The file at path: {arguments.file} does not exist, please provide a valid input file")
+            sys.exit(66)
+        if os.path.isdir(arguments.dest) and len(os.listdir(arguments.dest)) > 0:
+            print(f"The destination folder: {arguments.dest} exist and is not empty. You must call the program with a valid folder.")
+            sys.exit(73)
 
     def create_client(self):
         """Generate the python client from the arguments given
